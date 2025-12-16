62f5929506b79b00c4ca64e72bce8e13baee2a16
Aprapya Rana <118826832+DojaBiraloo@users.noreply.github.com>
2025-12-16 20:50:21 +0545

    Admin First Commit

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,8 @@ import Checkout from './components/Cart/Checkout'
 import OrderConfirmationPage from './pages/OrderConfirmationPage'
 import OrderDetailsPage from './pages/OrderDetailsPage'
 import MyOrderPage from './pages/MyOrderPage'
+import AdminLayout from './components/Admin/AdminLayout'
+import AdminHomePage from './pages/AdminHomePage'
 
 
 const App = () => {
@@ -31,6 +33,11 @@ const App = () => {
           <Route path='order/:id' element={<OrderDetailsPage />} />
           <Route path='my-orders' element={<MyOrderPage />} />
         </Route>
+
+        {/* Admin layout */}
+          <Route path='/admin' element={<AdminLayout />}>
+          <Route index element={<AdminHomePage />} />
+          </Route>
       </Routes>
     </BrowserRouter>
   )
